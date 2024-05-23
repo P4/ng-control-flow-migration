94bd5f7a4eb685739f9f1d863565358f7fb404d5
P4 <969645+P4@users.noreply.github.com>
2024-05-23 11:39:17 +0200

    add repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+  <ng-container [ngSwitch]="buttonVariant">
+    <ng-template ngSwitchCase=""><button mat-button>Button</button></ng-template>
+    <ng-template ngSwitchCase="raised"><button mat-raised-button>Button</button></ng-template>
+    <ng-template ngSwitchCase="stroked"><button mat-stroked-button>Button</button></ng-template>
+    <ng-template ngSwitchCase="flat"><button mat-flat-button>Button</button></ng-template>
+  </ng-container>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'ng-control-flow-migration';
+  buttonVariant: "" | "raised" | "stroked" | "flat" = "";
 }
